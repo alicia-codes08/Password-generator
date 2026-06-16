96e2f12a3798938488c60b989df58789e77f4f13
alicia <alicia@w>
2026-06-16 15:02:20 +0200

    Only password to use generator

--- generatinor.py
+++ generatinor.py
@@ -11,26 +11,7 @@ def using_password_gen(password_list):
         element = random.choice(elements_list)
         password_list.append(element)
 
-
-# Baustelle 
-def highest_element():
-    """Get the highest element."""
-    last_element = 0
-    for element in elements_list:
-        if element in use_password:
-            if elements_list.index(element) > last_element:
-                last_element = elements_list.index(element)
-    return last_element
-
-def lowest_element():
-    """Get the lowest element."""
-    first_element = 51
-    for element in elements_list:
-        if elements_list.index(element) < last_element:
-            first_element = elements_list.index(element)
-    return first_element
-
-     
+    
 # Generate a Password to use
 use_password = []
 using_password_gen(use_password)
