ff4429630d030b122e9cf60097301b9473c2385c
alicia <alicia@w>
2026-06-18 17:03:44 +0200

    Put code in generator.py in class

--- generator.py
+++ generator.py
@@ -1,66 +1,75 @@
 import random
 from pathlib import Path
 
-path = Path("password_elements.txt")
-elements_list = path.read_text().splitlines()
+
+class Generator():
+    """Class to manage the generator."""
+
+    def __init__(self):
+        """Initialize attributes and path."""
+        self.path = Path("password_elements.txt")
+        self.elements_list = self.path.read_text().splitlines()
+
+        self.highest_index = 0
+        self.lowest_index = 49
+
+        self.use_password = []
+        self.keep_password = []
+        self.moved_place_num = 0
 
 
-# Generate a Password to use
-def using_password_gen(password_list):
-    """Generate a password to use:"""
-    while len(password_list) < 8:
-        element = random.choice(elements_list)
-        if element=="A" and "-" in use_password:
-            continue
-        if element=="-" and "A" in use_password:
-            continue
-        password_list.append(element)
-    
-use_password = []
-using_password_gen(use_password)
-while len(use_password) < 8: 
-    using_password_gen()
+    def using_password_gen(self):
+        """Generate a password to use:"""
+        while len(self.use_password) < 8:
+            element = random.choice(self.elements_list)
+            if element=="A" and "-" in self.use_password:
+                continue
+            if element=="-" and "A" in self.use_password:
+                continue
+            self.use_password.append(element)
 
 
-# Generate password to keep
-def highest_index():
-    """Find out the highest index of the using-password."""
-    highest_index = 0
-    for element_up in use_password:
-        index = elements_list.index(element_up)
-        if index > highest_index:
-            highest_index = int(index)
-    
-    return highest_index
-
-def lowest_index():
-    """Find out the lowest index of the using_password."""
-    lowest_index = 49
-    for element_up in use_password:
-        index = elements_list.index(element_up)
-        if index < lowest_index:
-            lowest_index = int(index)
-    
-    return lowest_index
+    # Generate password to keep
+    def highest_lowest_index(self):
+        """Find out the highest index of the using-password."""
+        highest_index = self.highest_index
+        for element_up in self.use_password:
+            index = self.elements_list.index(element_up)
+            if index > highest_index:
+                highest_index = int(index)
         
-# get the highest and lowest index in your using-password 
-h_index = highest_index()
-l_index = lowest_index()
+        lowest_index = self.lowest_index
+        for element_up in self.use_password:
+            index = self.elements_list.index(element_up)
+            if index < lowest_index:
+                lowest_index = int(index)
 
-# getting range 
-right_max = 49 - h_index 
-left_max = l_index*-1
+        self.highest_index = highest_index
+        self.lowest_index = lowest_index
 
-# number of moved place for keeping-password 
-while True:
-    choose_num = random.randint(left_max, right_max) 
-    if choose_num != 0:
-        break
 
-# getting keeping-password
-keep_password = [] 
-for element in use_password:
-    index = elements_list.index(element)
-    new_index = index + choose_num 
-    new_element = elements_list[new_index]
-    keep_password.append(new_element) 
+    def moved_place(self):
+        """Getting number of moved place:"""
+        # get the highest and lowest index in your using-password 
+        h_index = self.highest_index
+        l_index = self.lowest_index 
+
+        # getting range 
+        right_max = 49 - h_index 
+        left_max = l_index*-1
+        # number of moved place for keeping-password 
+        while True:
+            choose_num = random.randint(left_max, right_max) 
+            if choose_num != 0:
+                break
+
+        self.moved_place_num = choose_num
+
+    def getting_keeping_password(self):
+        """Getting keeping password."""
+        # getting keeping-password
+        for element in self.use_password:
+            index = self.elements_list.index(element)
+            new_index = index + self.moved_place_num
+            new_element = self.elements_list[new_index]
+            self.keep_password.append(new_element) 
--- layout.py
+++ layout.py
@@ -2,16 +2,18 @@ import tkinter as tk
 import generator as gr
 
 # important variables
-u_password = gr.use_password
-k_password = gr.keep_password
-k_num = gr.choose_num
+generator = gr.Generator()
+generator.using_password_gen()
+generator.highest_lowest_index()
+generator.moved_place()
+generator.getting_keeping_password()
+u_password = generator.use_password
+k_password = generator.keep_password
+k_num = generator.moved_place_num
 
-r_max = gr.right_max
-l_max = gr.left_max 
 
 ### print-tests
 print(f"using-password: {u_password}")
-print(f"range: right=> {r_max}, left=> {l_max}")  
 print(f"move: {k_num}") 
 print(f"keeping-password: {k_password}") 
 
