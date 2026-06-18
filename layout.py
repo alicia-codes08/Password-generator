9a0778007fec6a6bc078b807bbe8dc1838d13c1e
alicia <alicia@w>
2026-06-18 14:20:52 +0200

    Working AC-button

--- layout.py
+++ layout.py
@@ -23,7 +23,6 @@ white = "#F9F9F9"
 red = "#F01111"
 grey = "#A7A7A7"
 
-
 # setting up the window and frames
 window = tk.Tk()
 window.title("Using/keeping-password generator")
@@ -33,22 +32,36 @@ window.config(background=blue)
 
 frame = tk.Frame(window) 
 
-label_1 = tk.Label(window, text="using password", font=("Arial", 25),
+
+label_1 = tk.Label(window, text="", font=("Arial", 25),
                    relief="solid")
 label_1.place(x=170, y=50, width=290, height=70)
 
-label_2 = tk.Label(window, text="keeping password", font=("Arial", 25),
+label_2 = tk.Label(window, text="", font=("Arial", 25),
                    relief="solid")
-label_2.place(x=170, y=300, width=290, height=70)
+label_2.place(x=170, y=165, width=290, height=70)
+
+label_3 = tk.Label(window, text="", font=("Arial", 25),
+                   relief="solid")
+label_3.place(x=200, y=330, width=210, height=70)
+
+# global variables
+label_1["text"] = "ass"
+label_2["text"] = "ass"
+label_3["text"] = "ass"
+
+# clear
+def clear():
+    """Clear all."""
+    label_1["text"] = "using password"
+    label_2["text"] = "keeping password"
+    label_3["text"] = "moved place"
 
 # act after klicking a button 
 def klicked(value):
     """acting after clicking buttons"""
-    if value == "PRESS":
+    if True:
         pass
-    elif value == "AC":
-        pass 
-
 
 button_press = tk.Button(window, text="PRESS", font=("Arial", 30), 
                          command=lambda: klicked("PRESS"))
@@ -56,12 +69,9 @@ button_press.config(foreground=black, background=grey, relief="groove")
 button_press.place(x=10, y=590, anchor="sw")
 
 button_ac = tk.Button(window, text="AC", font=("Arial", 30), 
-                         command=lambda: klicked("AC"))
-button_ac.config(foreground=black, background=red, relief="flat")
-button_ac.place(x=590, y=590, anchor="se") 
-
-    
-
+                         command=lambda: clear())
+button_ac.config(foreground=black, background=red, relief="groove")
+button_ac.place(x=590, y=590, anchor="se")    
 
 frame.pack() 
 window.mainloop() 
